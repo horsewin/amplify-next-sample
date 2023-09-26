1fd4fe9b3857c6d56f4426f277c999cbf3f01e77
horsewin <mamamatz@gmail.com>
2023-09-26 15:24:42 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,17 @@
 import Image from 'next/image'
 
-// @ts-ignore
-export default function Home({ formattedDate }) {
-  return (
-    <>
-      <h1>Static page</h1>
-      <p>This page is static. It was built on {formattedDate}.</p>
-      <p>
-        <a href="/ssr">View a server-side rendered page.</a>
-      </p>
-    </>
-  )
+export default async function Page() {
+  const data = await getData()
+ 
+  return <main></main>
 }
 
-export async function getStaticProps() {
+async function getData() {
   const buildDate = Date.now();
   const formattedDate = new Intl.DateTimeFormat("en-US", {
     dateStyle: "long",
     timeStyle: "long",
   }).format(buildDate);
-
-  return { props: { formattedDate } };
+ 
+  return { formattedDate } 
 }
